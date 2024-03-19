5acf46253686a6e3d7ed42a60308faa68ca395a1
Ihor <mrshevchenko33@gmail.com>
2024-03-19 21:50:29 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,14 @@
+def difference(*args):
+    if not args:
+        return 0
+    else:
+        min_val = min(args)
+        max_val = max(args)
+        return round(max_val - min_val, 2)
+
+
+assert difference(1, 2, 3) == 2, 'Test1'
+assert difference(5, -5) == 10, 'Test2'
+assert difference(10.2, -2.2, 0, 1.1, 0.5) == 12.4, 'Test3'
+assert difference() == 0, 'Test4'
+print('OK')
